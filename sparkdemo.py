a6720ec7a6b0dba104169444470d325cd9ff4cd9
SUMITRANE163 <sumitrane1603@gmail.com>
2019-04-27 02:01:47 -0500

    Initial commit

---
+++ sparkdemo.py
@@ -0,0 +1,4 @@
+from pyspark import SparkConf, SparkContext
+
+sc = SparkContext(master="local", appName="Spark Demo")
+print(sc.textFile("/Users/sumitrane163/server/spark-2.4.1-bin-hadoop2.7/README.md").first())
