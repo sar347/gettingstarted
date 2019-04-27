47e3c812422c64e6aef3987ea87badb800a507ea
SUMITRANE163 <sumitrane1603@gmail.com>
2019-04-27 02:10:58 -0500

    added one comment line

--- sparkdemo.py
+++ sparkdemo.py
@@ -2,3 +2,5 @@ from pyspark import SparkConf, SparkContext
 
 sc = SparkContext(master="local", appName="Spark Demo")
 print(sc.textFile("/Users/sumitrane163/server/spark-2.4.1-bin-hadoop2.7/README.md").first())
+
+#commit test
